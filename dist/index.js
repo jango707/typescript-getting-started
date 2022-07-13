83e6efb337a7fe56d6ca45b42519e57a800a7a2d
jango707 <jangbel99@gmail.com>
2022-07-13 17:01:14 +0100

    setting up

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+let sales = 1234567890;
+let course = 'TypeScript';
+let is_published = true;
+let level;
+level = 10;
+level = '10';
+let nums = [1, 2, 2, 3, 3];
+nums.forEach(n => n.toExponential(8));
+//# sourceMappingURL=index.js.map
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+let sales: number = 1_234_567_890;
+let course: string = 'TypeScript';
+let is_published: boolean = true;
+
+let level;
+
+level = 10
+level = '10'
+
+let nums: number[] = [1,2,2,3,3]
+
+nums.forEach(n => n.toExponential(8))
